93a07d9d1509e7f49ec5054a4b8ffd040128fe59
Aristotle Lavayna <aristotlelavayna@gmail.com>
2024-01-02 22:13:12 +0800

    commit

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,16 +1,3 @@
-console.log('hello');
-
-let titleDiv = document.getElementById('title')
-console.log(title);
-
-console.log('before: ', title.innerHTML);
-title.innerText = 'goodbye'
-console.log('after: ',title.innerText);
-
-let message = 'goodbye my lover'
-titleDiv.innerHTML = `'<p>${message}</p>'`
-
-titleDiv.style.color = 'red'
 
 ////////////////
 let redDiv = document.getElementById('red')
@@ -26,12 +13,28 @@ redDiv.onclick = () => console.log('you clicked red');
 yellowDiv.onclick = () => console.log('you clicked yellow');
 greenDiv.onclick = () => console.log('you clicked green');
 
-
-
+             /* RED YELLOW GREEN GAME*/
 // forEach loop
 const squares = document.querySelectorAll('.colorSquare')
+const timesClicked = {'red': 0, 'yellow': 0, 'green': 0}
+
 squares.forEach(square => console.log(square.value));
 
 squares.forEach(square => {
-    square.onclick = () => console.log(square.value);
+    square.onclick = () => {
+        timesClicked[square.value] += 1
+        square.innerText = timesClicked[square.value]
+    }
 });
+
+function clearScores() {
+    timesClicked.red = 0;
+    timesClicked.yellow = 0;
+    timesClicked.green = 0;
+    squares.forEach(square => {
+        square.innerText = 0
+    })
+}
+
+const clearGameBtn = document.getElementById('clear-game')
+clearGameBtn.onclick = () => clearScores()
